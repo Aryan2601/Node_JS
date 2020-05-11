b5439a34708845da03477549a2b94c6ce6f0d784
FuryAryan <yoyo26new@gmail.com>
2020-05-11 19:09:30 +0530

    added non-blocking asynchronous way

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
  const fs = require('fs');
-
+ //Blocking , Synchronous way
  const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
  console.log(textIn);
-
  const textOut = `This is what we know about avacado : ${textIn}.\nCreated on ${Date.now()}`;
  fs.writeFileSync('./txt/output.txt', textOut);
  console.log('File Written');
+
+ // Non-blocking Asynchronous way
+ fs.readFile('./txt/start.txt');
